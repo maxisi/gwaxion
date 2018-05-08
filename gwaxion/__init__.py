eb94702c46e3895f260d0caf62314ae62de59c83
Max Isi <max.isi@ligo.org>
2018-05-07 19:35:58 -0700

    SWSHs

---
+++ gwaxion/__init__.py
@@ -0,0 +1,13 @@
+# import matplotlib
+# matplotlib.use('Agg')
+# 
+# mplparams = {
+#     'text.usetex': True,  # use LaTeX for all text
+#     'axes.linewidth': 1,  # set axes linewidths to 0.5
+#     'axes.grid': False,  # add a grid
+#     'axes.labelweight': 'normal',
+#     'font.family': 'serif',
+#     'font.size': 24,
+#     'font.serif': 'Computer Modern Roman'
+# }
+# matplotlib.rcParams.update(mplparams)
