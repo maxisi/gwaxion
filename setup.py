646f7fc03a3efbd30a98ef9e88957edff55afa0b
max.isi <max.isi@ligo.org>
2020-12-28 16:47:47 -0300

    fixed url

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
     description="Black-hole boson gravitational-wave package.",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/pypa/sampleproject",
+    url="https://github.com/maxisi/gwaxion",
     packages=setuptools.find_packages(),
     classifiers=[
         "Intended Audience :: Science/Research",
